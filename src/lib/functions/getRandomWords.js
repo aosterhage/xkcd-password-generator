4a930c8b1737e735fd444b59aaae1b8a3b1adcb3
aosterhage <aaron.osterhage@gmail.com>
2022-02-23 19:25:21 +0000

    Refactor JavaScript code to separate files
    Also change from using Math.random() to crypto.getRandomValues() for
    getting random values. The Web Crypto API is more secure and since this
    is being used for a password generator its better to be secure!

---
+++ src/lib/functions/getRandomWords.js
@@ -0,0 +1,27 @@
+import { dictionary } from "../data/dictionary.js";
+
+export function getRandomWords(numberOfWords) {
+    let indices = getRandomValues(numberOfWords, dictionary.length);
+
+    let words = [];
+    indices.forEach(index => words.push(dictionary[index]));
+    return words;
+}
+
+function getRandomValues(numberOfValues, maxValue) {
+    // using Uint16Array means that maxValue is limimted to max(Uint16)
+    if (maxValue >= 65536) {
+        return;
+    }
+
+    // populate array with random values
+    let cryptoArray = new Uint16Array(numberOfValues);
+    self.crypto.getRandomValues(cryptoArray);
+
+    // make sure that all random values are less than maxValue
+    cryptoArray.forEach(function(value, index, array) {
+        array[index] = value % maxValue;
+    });
+
+    return cryptoArray;
+}
